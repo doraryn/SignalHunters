b9ed03b7890a178f19d745dc929858a78d0e6886
doraryn <dora.magyary@stud.ubbcluj.ro>
2026-04-01 16:48:35 +0300

    added more features

---
+++ scripts/killzone.gd
@@ -0,0 +1,7 @@
+extends Area2D
+@onready var game_manager: Node = $"../GameManager"
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		body.respawn()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 @onready var grapple_controller = $GrappleControler
 @onready var death_sound: AudioStreamPlayer2D = $DeathSound
+@onready var game_manager: Node = $"../GameManager"
 
 
 const SPEED = 300.0
@@ -10,17 +11,37 @@ const JUMP_VELOCITY = -850.0
 const ACCELERATION = 0.1
 const DECELERATION = 0.1
 
-var alive = true
+var took_damage = false
+var can_move = true
 
+func respawn():
+	self.visible = false
+	can_move = false
+	await get_tree().create_timer(0.5).timeout # time until respwan
+	
+	self.global_position = Vector2(87, 563)    # respwan location
+	self.visible = true
+	can_move = true
+	
+	await get_tree().create_timer(0.5).timeout # time until respwan
+	
+	took_damage = false
    
 func _physics_process(delta: float) -> void:
-	
-	if !alive:
-		return
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	# Collision checking e.g. if touching spikes
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		
+		if collision.get_collider().name == "TileMapSpikes": # if player touches anything drawn in tilemapsikes
+			if took_damage == false:
+				took_damage = true # set damage as true
+				game_manager.update_player_health(-1) 
+				respawn() # respawn the player
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") && (is_on_floor() || grapple_controller.launched):
@@ -37,18 +58,17 @@ func _physics_process(delta: float) -> void:
 		
 	#Add animation
 	if velocity.x > 1 or velocity.x < -1:
-		animated_sprite_2d.animation = "idle"
+		animated_sprite_2d.animation = "moving"
 	else:
 		animated_sprite_2d.animation = "idle"
 
 	move_and_slide()
 	
-	if direction == 1.0:
-		animated_sprite_2d.flip_h = false
-	elif direction == -1.0:
-		animated_sprite_2d.flip_h = true
-		
-func die() -> void:
-	death_sound.play()
-	animated_sprite_2d.animation = "dying"
-	alive = false
+	if can_move == false:
+		death_sound.play()
+		return
+	else:
+		if direction == 1.0:
+			animated_sprite_2d.flip_h = false
+		elif direction == -1.0:
+			animated_sprite_2d.flip_h = true
